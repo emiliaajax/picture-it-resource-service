b13ac70afaa422e463c5535d88c85a76741717ba
Emilia Hansson <eh222yn@student.lnu.se>
2022-03-11 16:55:06 +0100

    Start implement routers and add JWT authentication

---
+++ src/routes/api/v1/images-router.js
@@ -0,0 +1,48 @@
+/**
+ * Images routes.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+// import { ImagesController } from '../../../controllers/api/images-controller.js'
+
+export const router = express.Router()
+
+// const controller = new ImagesController()
+
+/**
+ * Authenticates the request.
+ *
+ * @param {object} req Express request object.
+ * @param {object} res Express response object.
+ * @param {Function} next Express next middleware function.
+ */
+const authenticateJWT = (req, res, next) => {
+  try {
+    const [authenticationScheme, token] = req.headers.authorization?.split(' ')
+
+    if (authenticationScheme !== 'Bearer') {
+      throw new Error('Invalid authentication scheme')
+    }
+
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    req.user = {
+      username: payload.sub,
+      firstName: payload.given_name,
+      lastName: payload.family_name,
+      email: payload.email
+    }
+
+    next()
+  } catch (error) {
+    const err = createError(403)
+    err.cause = error
+    next(err)
+  }
+}
+
+router.get('/', authenticateJWT)
